76c8484f77ed8615a42c41a644905deca54ee815
ViggaD <danhkvtse172932@gmail.com>
2024-09-02 23:46:20 +0700

    first update !!!

---
+++ cleaninghub/src/main/java/com/fpu/exe/cleaninghub/CleaninghubApplication.java
@@ -0,0 +1,13 @@
+package com.fpu.exe.cleaninghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaninghubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaninghubApplication.class, args);
+	}
+
+}
