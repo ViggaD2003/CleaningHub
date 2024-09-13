866743a4a7282179aa4accd49905895e5b40ffb9
nam127 <buithanhnam9109@gmail.com>
2024-09-13 20:01:17 +0700

    feat: Implement create booking feature

--- cleaninghub/src/main/java/com/fpu/exe/cleaninghub/controller/BookingController.java
+++ cleaninghub/src/main/java/com/fpu/exe/cleaninghub/controller/BookingController.java
@@ -2,7 +2,6 @@ package com.fpu.exe.cleaninghub.controller;
 
 import com.fpu.exe.cleaninghub.dto.request.CreateBookingRequest;
 import com.fpu.exe.cleaninghub.dto.response.CreateBookingResponse;
-import com.fpu.exe.cleaninghub.dto.response.CreateServiceResponseDto;
 import com.fpu.exe.cleaninghub.dto.response.BookingDetailResponseDto;
 import com.fpu.exe.cleaninghub.dto.response.BookingResponseDto;
 import com.fpu.exe.cleaninghub.services.interfc.BookingService;
@@ -15,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
