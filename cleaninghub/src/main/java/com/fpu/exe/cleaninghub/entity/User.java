35859089819260b5dde5663de13f9c306f057485
minhuy2411 <hoanganh241115@gmail.com>
2024-10-04 00:34:49 +0700

    feat: update get all service and update service (clean code)

--- cleaninghub/src/main/java/com/fpu/exe/cleaninghub/entity/User.java
+++ cleaninghub/src/main/java/com/fpu/exe/cleaninghub/entity/User.java
@@ -9,14 +9,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Date;
