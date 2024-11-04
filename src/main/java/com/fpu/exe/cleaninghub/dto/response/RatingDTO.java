9cd477cc6ee9336c48e82f68692d8dd7cc3aeb54
ViggaD <danhkvtse172932@gmail.com>
2024-11-05 01:33:46 +0700

    fix RatingDTO no contructor again

--- src/main/java/com/fpu/exe/cleaninghub/dto/response/RatingDTO.java
+++ src/main/java/com/fpu/exe/cleaninghub/dto/response/RatingDTO.java
@@ -1,13 +1,17 @@
 package com.fpu.exe.cleaninghub.dto.response;
 
 import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
-@Builder
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RatingDTO {
     private Long id;
 
@@ -16,4 +20,12 @@ public class RatingDTO {
     private LocalDate ratingDate;
 
     private String comments;
+
+    @Builder // Đặt @Builder tại constructor có tất cả các tham số
+    public RatingDTO(Long id, Integer stars, LocalDate ratingDate, String comments) {
+        this.id = id;
+        this.stars = stars;
+        this.ratingDate = ratingDate;
+        this.comments = comments;
+    }
 }
