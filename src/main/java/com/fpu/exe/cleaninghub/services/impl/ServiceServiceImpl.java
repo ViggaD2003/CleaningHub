e2999e3edd004ca418cddd31bfe82029eb388e28
ViggaD2003 <thanhdanhvt2003@gmail.com>
2024-11-06 00:27:25 +0700

    Update ServiceServiceImpl.java

--- src/main/java/com/fpu/exe/cleaninghub/services/impl/ServiceServiceImpl.java
+++ src/main/java/com/fpu/exe/cleaninghub/services/impl/ServiceServiceImpl.java
@@ -107,9 +107,10 @@ public class ServiceServiceImpl implements ServiceService {
         }).collect(Collectors.toList());
     }
 
-    @Override
+     @Override
     public void updateImgService(Integer serviceId, String imgURL) {
         com.fpu.exe.cleaninghub.entity.Service service = serviceRepository.findById(serviceId).orElseThrow(() -> new RuntimeException("Service not found"));
+        imgURL = imgURL.replace("\"", "").trim();
         service.setImg(imgURL);
         serviceRepository.save(service);
     }
