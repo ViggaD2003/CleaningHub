1f0b3990134cbbf1b52f92889a8da68c64edcfa0
ViggaD2003 <thanhdanhvt2003@gmail.com>
2024-11-06 22:34:46 +0700

    Update PayOSController.java

--- src/main/java/com/fpu/exe/cleaninghub/controller/PayOSController.java
+++ src/main/java/com/fpu/exe/cleaninghub/controller/PayOSController.java
@@ -35,7 +35,7 @@ public class PayOSController {
     }
     @GetMapping
     public RedirectView successOrCancel(@RequestParam Integer bookingId, @RequestParam String status){
-        if (status.equalsIgnoreCase("CANCELED")){
+        if (status.equalsIgnoreCase("CANCELLED")){
             bookingService.changePaymentStatusOfBooking(orderCode, bookingId, PaymentStatus.FAILED);
             return new RedirectView("https://cleaning-hub.vercel.app/booking-cancel");
         }
