c2d88db4604a95a60d44270e1c5439d7cfcc3d3b
AnataAria <kakata28419@gmail.com>
2024-11-08 20:16:07 +0700

    fix and test bug on google authentication

--- src/main/java/com/fpu/exe/cleaninghub/config/ApplicationAuditing.java
+++ src/main/java/com/fpu/exe/cleaninghub/config/ApplicationAuditing.java
@@ -1,33 +1,50 @@
 package com.fpu.exe.cleaninghub.config;
 
 import com.fpu.exe.cleaninghub.entity.User;
+import com.fpu.exe.cleaninghub.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
 import java.util.Optional;
 
 @Configuration
 public class ApplicationAuditing implements AuditorAware<Integer> {
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public Optional<Integer> getCurrentAuditor() {
         Authentication authentication =
                 SecurityContextHolder
                         .getContext()
                         .getAuthentication();
-        if(authentication == null || !authentication.isAuthenticated()
-                || authentication instanceof AnonymousAuthenticationToken){
-            return Optional.empty();
-        }
+        
+    if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+        return Optional.empty();
+    }
 
-        User userPrincipal = (User) authentication.getPrincipal();
+    Object principal = authentication.getPrincipal();
+    if (principal instanceof User) {
+        User userPrincipal = (User) principal;
         return Optional.ofNullable(userPrincipal.getId());
+    }
+    else if (principal instanceof DefaultOAuth2User) {
+        DefaultOAuth2User oauthUser = (DefaultOAuth2User) principal;
+        String email = oauthUser.getAttribute("email");
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        return userOptional.map(User::getId);
+    }
+    return Optional.empty();
     }
 
     @Bean
-    public AuditorAware<Integer> auditorProvider() {
+    AuditorAware<Integer> auditorProvider() {
         return new ApplicationAuditing();
     }
 }
